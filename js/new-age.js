1268648b62df333942238df75e2fb3e3df9cf4bf
rodrigo3d <me@rodrigo3d.com>
2018-07-15 17:22:26 -0300

    [Atualização Automática]

--- js/new-age.js
+++ js/new-age.js
@@ -35,7 +35,7 @@
     }
   };
   // Collapse now if page is not at top
-  //navbarCollapse();
+  navbarCollapse();
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
